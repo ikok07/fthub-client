a65d39bc6f75e0aa64089f226d94cc2064d39622
Kaloyan Stefanov <kokmarok@gmail.com>
2023-08-14 14:31:38 +0300

    Initial Commit

---
+++ FTHub/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FTHub
+//
+//  Created by Kaloyan Petkov on 14.08.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FTHub/FTHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  FTHubApp.swift
+//  FTHub
+//
+//  Created by Kaloyan Petkov on 14.08.23.
+//
+
+import SwiftUI
+
+@main
+struct FTHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
