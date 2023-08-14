93665c5058abdaad07ddccb56329c6f4818b0530
Kaloyan Stefanov <kokmarok@gmail.com>
2023-08-14 14:52:49 +0300

    Setup assets for tutorial section

---
+++ FTHub/Utils/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  FTHub
+//
+//  Created by Kaloyan Petkov on 14.08.23.
+//
+
+import Foundation
+import SwiftUI
+
+struct K {
+    
+    // MARK: - Gradients
+    static let mainGradient: Gradient = Gradient(colors: [.accentColor, .mint])
+    
+}
