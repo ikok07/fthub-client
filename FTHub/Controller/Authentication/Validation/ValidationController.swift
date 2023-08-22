8598feee8c7fff972bb253bbd4612dae33524166
marto171 <georgievmarti21@gmail.com>
2023-08-22 16:29:35 +0300

    create validator and first 2 base validation methods

--- FTHub/Controller/Authentication/Validation/ValidationController.swift
+++ FTHub/Controller/Authentication/Validation/ValidationController.swift
@@ -8,7 +8,6 @@
 import Foundation
 
 class ValidationController {
-    
     static func validateName(name: String) -> (valid: Bool, message: String?) {
         if name == "" {
             return (false, "Please provide a name!")
@@ -18,12 +17,36 @@ class ValidationController {
     }
     
     static func validateEmail(email: String) -> (valid: Bool, message: String?) {
+        if (email == "") {
+            return (false, "Please provide an email!")
+        }
         
+        let isEmail = Validator.isEmail(str: email)
         
-        return (true, nil)
+        return (isEmail, isEmail == true ? nil : "Not a valid email.")
     }
     
     static func validatePassword(password: String, passwordConfirm: String) -> (valid: Bool, message: String?) {
+        if (password == "") {
+            return (false, "Please provide a password!")
+        }
+        
+        if (passwordConfirm == "") {
+            return (false, "Please confirm your password!")
+        }
+        
+        if (password.count >= 8) {
+            return (false, "Your password must be at least 8 characters long!")
+        }
+        
+        let containsLowercase = Validator.containsLowercaseCharacter(str: password)
+        
+        let containsUppercase = Validator.containsUppercaseCharacter(str: password)
+        
+        let containsNumber = Validator.containsDigit(str: password)
+        
+        let containsCustomSymbol = Validator.containsSymbol(str: password)
+        
         return (true, nil)
     }
     
