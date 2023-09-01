354db5146a1cb4aa30443604f09eec918e915ba6
ikok07 <kokmarok@gmail.com>
2023-09-01 14:40:29 +0300

    make changes to HealthKitController

--- FTHub/Controller/HealthKit/HealthKitController.swift
+++ FTHub/Controller/HealthKit/HealthKitController.swift
@@ -18,21 +18,46 @@ class HealthKitController: ObservableObject {
     @Published var height = HKQuantityType.quantityType(forIdentifier: .height)!
     @Published var weight = HKQuantityType.quantityType(forIdentifier: .bodyMass)!
     
+    @Published var query: HKStatisticsCollectionQuery?
+    @Published var quantityTypes: Set<HKSampleType>?
+    
     init() {
         if HKHealthStore.isHealthDataAvailable() {
             self.healthStore = HKHealthStore()
         } else {
+            print("HealthKit is not supported on this device")
+        }
+        self.quantityTypes = [exerciseTime, activeEnergyBurned, bodyFatPercentage, bmi, height, weight]
+    }
+    
+    func requestAuthorization() -> Bool {
+        
+        var authStatus: Bool?
+        
+        healthStore?.requestAuthorization(toShare: self.quantityTypes, read: self.quantityTypes) { success, error in
+            if success {
+                authStatus = true
+            } else {
+                print(error ?? "")
+                authStatus = false
+            }
+        }
+        
+        return authStatus ?? false
+    }
+    
+    func getData() {
+        
+        let predicate = HKQuery.predicateForSamples(withStart: Date(), end: nil)
+        
+        self.query = HKStatisticsCollectionQuery(quantityType: weight, quantitySamplePredicate: predicate, anchorDate: Date(timeIntervalSinceNow: 300), intervalComponents: DateComponents(day: 1))
+        
+        self.query?.initialResultsHandler = { query, statisticsCollection, error in
             
         }
     }
     
-    func requestAuthorization() {
-        
-        
-        healthStore?.requestAuthorization(toShare: [exerciseTime, activeEnergyBurned, bodyFatPercentage, bmi, height, weight], read: nil) { success, error in
-            
-        }
-        
-    }
+    
+    
     
 }
