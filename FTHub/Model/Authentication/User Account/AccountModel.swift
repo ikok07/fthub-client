921607ed5271acb807462c03e12c63cb96c53dce
ikok07 <kokmarok@gmail.com>
2023-09-03 21:08:55 +0300

    fix bug with details checking

--- FTHub/Model/Authentication/User Account/AccountModel.swift
+++ FTHub/Model/Authentication/User Account/AccountModel.swift
@@ -34,27 +34,22 @@ struct AccountModel {
         let response = await Authentication.checkDetails(token)
         defaults.setValue(false, forKey: "loadingPresented")
         
+        var newDetails = UserDetails(setupActivePage: 0)
+        
         if let safeResponse = response {
             if safeResponse.status == "success" {
                 defaults.setValue(true, forKey: "hasDetails")
-                let newDetails = UserDetails(setupActivePage: 0)
                 newDetails.age = safeResponse.data.userDetails.age
                 newDetails.gender = Gender(rawValue: safeResponse.data.userDetails.gender!)
                 newDetails.goal = FitnessGoal(rawValue: safeResponse.data.userDetails.goal!.camelCased)
                 newDetails.height = safeResponse.data.userDetails.height
                 newDetails.weight = safeResponse.data.userDetails.weight
                 newDetails.workoutsPerWeek = safeResponse.data.userDetails.trainingFrequencyPerWeek
-                return newDetails
-            } else {
-                defaults.setValue(false, forKey: "hasDetails")
             }
         } else {
-            // За да не забравя: Умишлено го правя да излиза от профила, за да е по-сигурно
-            defaults.setValue(false, forKey: "userLoggedIn")
             defaults.setValue(false, forKey: "hasDetails")
-            Message.send(type: "error", message: "There was an error connecting to our servers. Please try again later.")
         }
-        return nil
+        return newDetails
     }
     
 }
