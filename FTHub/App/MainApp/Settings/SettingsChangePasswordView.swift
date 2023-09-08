f92b2cd2ed85bc03a852ed6e456772b79e563589
ikok07 <kokmarok@gmail.com>
2023-09-08 12:15:46 +0300

    Design change password settings page

---
+++ FTHub/App/MainApp/Settings/SettingsChangePasswordView.swift
@@ -0,0 +1,50 @@
+//
+//  SettingsChangePasswordView.swift
+//  FTHub
+//
+//  Created by Kaloyan Petkov on 8.09.23.
+//
+
+import SwiftUI
+
+struct SettingsChangePasswordView: View {
+    
+    @State private var currentPassword: String = ""
+    @State private var newPassword: String = ""
+    @State private var confirmPassword: String = ""
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                VStack(spacing: 20) {
+                    CustomTextFieldView(icon: "key.horizontal", placeholder: "Current password", autoCapitalize: false, secureField: true, disableAutoCorrect: true, type: .password, text: $currentPassword)
+                    
+                    CustomTextFieldView(icon: "key.horizontal", placeholder: "New password", autoCapitalize: false, secureField: true, disableAutoCorrect: true, type: .password, text: $newPassword)
+                    
+                    CustomTextFieldView(icon: "key.horizontal", placeholder: "Confirm new password", autoCapitalize: false, secureField: true, disableAutoCorrect: true, type: .password, text: $confirmPassword)
+                }
+                
+                Spacer()
+                
+                Button(action: changePassword) {
+                       Text("Change password")
+                        .padding(EdgeInsets(top: 10, leading: 0, bottom: 10, trailing: 0))
+                }
+                .buttonStyle(CTAButtonStyle(gradient: K.Gradients.mainGradient))
+
+            }
+            .navigationTitle("Change Password")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+        .padding()
+    }
+    
+    func changePassword() {
+        
+    }
+}
+
+#Preview {
+    SettingsChangePasswordView()
+        .padding()
+}
--- FTHub/App/MainApp/Settings/SettingsMainView.swift
+++ FTHub/App/MainApp/Settings/SettingsMainView.swift
@@ -32,7 +32,9 @@ struct SettingsMainView: View {
                             SettingsRowView(icon: "person", image: nil, label: "Profile Data")
                         }
                         SettingsRowView(icon: "star", image: nil, label: "Subscription")
-                        SettingsRowView(icon: "key.horizontal", image: nil, label: "Change Password")
+                        NavigationLink(destination: SettingsChangePasswordView()) {
+                            SettingsRowView(icon: "key.horizontal", image: nil, label: "Change Password")
+                        }
                         SettingsRowView(icon: "clock.arrow.2.circlepath", image: nil, label: "Workouts Synchronization")
                         SettingsRowView(icon: "bell", image: nil, label: "Notifications")
                         SettingsRowToggleView(icon: "heart", label: "Apple Health", isToggled: $appleHealth)
