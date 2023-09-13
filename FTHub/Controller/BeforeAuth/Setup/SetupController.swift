e91256d5a8c756ae7f1ea03a5f6358ae65c88d10
ikok07 <kokmarok@gmail.com>
2023-09-13 12:21:53 +0300

    fix small bug with units in setup

--- FTHub/Controller/BeforeAuth/Setup/SetupController.swift
+++ FTHub/Controller/BeforeAuth/Setup/SetupController.swift
@@ -15,7 +15,7 @@ enum Unit: String, CaseIterable, Codable {
 class SetupController: ObservableObject {
     
     @Published var activePage: Int = 0
-    @Published var units: Unit?
+    @Published var units: Unit = .metric
     
     @Published var gender: Gender? = .Male
     @Published var age: Int?
@@ -25,7 +25,7 @@ class SetupController: ObservableObject {
     @Published var goal: FitnessGoal?
     
     func saveDetailsToServer(completion: ((Bool) -> Void)? = nil) async {
-        await SetupModel.saveDetailsToServer(age: self.age ?? 18, height: self.height ?? 180, weight: self.weight ?? 70, workoutsPerWeek: self.workoutsPerWeek ?? 3, gender: self.gender?.rawValue ?? Gender.Male.rawValue, units: units ?? .metric, goal: self.goal?.rawValue ?? FitnessGoal.Balance.rawValue, completion: completion)
+        await SetupModel.saveDetailsToServer(age: self.age ?? 18, height: self.height ?? 180, weight: self.weight ?? 70, workoutsPerWeek: self.workoutsPerWeek ?? 3, gender: self.gender?.rawValue ?? Gender.Male.rawValue, units: units, goal: self.goal?.rawValue ?? FitnessGoal.Balance.rawValue, completion: completion)
     }
     
 }
